a3028561a5dd369e2315623c1cdedb26909143c5
Oemer Genc <oemergenc@gmail.com>
2019-06-30 15:22:43 +0200

    fixed build script

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-cd sample.mvn.parent
 
 ./mvnw dependency:get -Dartifact=at.bestsolution:tycho-lifecycle-controller:0.0.2-SNAPSHOT -DrepoUrl=http://maven.bestsolution.at/efxclipse-snapshots/
 ./mvnw dependency:get -Dartifact=at.bestsolution:maven-osgi-targetplatform-extension:0.0.2-SNAPSHOT -DrepoUrl=http://maven.bestsolution.at/efxclipse-snapshots/
 ./mvnw dependency:get -Dartifact=at.bestsolution:maven-osgi-package-plugin:0.0.2-SNAPSHOT -DrepoUrl=http://maven.bestsolution.at/efxclipse-snapshots/
 
-./mvnw clean package
+cd sample.mvn.parent
+
+../mvnw clean package

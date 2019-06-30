7f2d4dc3b98de208a48c38a4ba7eb2283aa84084
Oemer Genc <oemergenc@gmail.com>
2019-06-30 15:27:09 +0200

    fixed build script

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@
 ./mvnw dependency:get -Dartifact=at.bestsolution:maven-osgi-targetplatform-extension:0.0.2-SNAPSHOT -DrepoUrl=http://maven.bestsolution.at/efxclipse-snapshots/
 ./mvnw dependency:get -Dartifact=at.bestsolution:maven-osgi-package-plugin:0.0.2-SNAPSHOT -DrepoUrl=http://maven.bestsolution.at/efxclipse-snapshots/
 
-./mvnw clean package -f sample.mvn.paren/pom.xml
+./mvnw clean package -f sample.mvn.parent/pom.xml
